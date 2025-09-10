bb233eb4c4375aa5c59e84eedc1cd78a34ff6e88
Fabian Nobert <fabiannobert.007@gmail.com>
2025-09-10 17:13:58 +0100

    Add completion of exercise 2.6, adding names to phonebook

---
+++ phoneBook/src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+
+const Number = (props) =>{
+console.log('props value', props)
+const {person} = props
+return (
+  <>
+ <div> {person.name}</div>
+  </>
+)
+}
+
+function App() {
+const [persons, setPersons] = useState([{name:'Fabian Nobert'}])
+const [newName, setNewName] = useState('a new name...')
+console.log('list of persons', persons)
+
+
+
+
+const handleAdd = (e) => {
+e.preventDefault()
+console.log(e.target)
+const namesObject = {
+name: newName
+}
+setPersons(persons.concat(namesObject))
+setNewName('')
+
+}
+
+const handleNewName = (e)=> {
+const inputValue = e.target.value
+console.log('new name input', inputValue)
+setNewName(inputValue)
+}
+
+  
+  return (
+    <>
+    <h2>Phonebook</h2>
+    <form onSubmit={handleAdd}>
+      <div>
+      name: <input
+      value = {newName}
+      onChange={handleNewName}
+      />
+      </div>
+      <div><button type='submit'>add</button></div>
+    </form>
+
+    <div>
+    <h2>Numbers</h2>
+    {persons.map(person => 
+    <Number key = {person.name} person = {person}/>
+    )}
+
+    </div>
+    </>
+  )
+}
+
+export default App
